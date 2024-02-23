d2fbffa4789d76c6fc1cb006f777c8d2e6dcffc9
Dirk <drudolph@adobe.com>
2024-02-23 21:59:58 +0100

    feat: link quote to quotee

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,26 @@
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
+export default async function decorate(block) {
+  const [quoteWrapper, authorWrapper] = block.children;
  
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
+
+  let quotee = authorWrapper.textContent.trim();
+  // request the /authors.json index
+  let quotees = await fetch('/authors.json');
+  if (quotees.ok) {
+    // get the returned json as javascript object
+    quotees = await quotees.json();
+    // find the first entry that's title matches our quotee
+    quotee = quotees.data.find(({ title }) => {
+      return title.toLowerCase() === quotee.toLowerCase();
+    })
+    if (quotee) {
+      const { path } = quotee;
+      const a = document.createElement('a');
+      a.href = path;
+      a.append(...authorWrapper.firstElementChild.childNodes);
+      authorWrapper.firstElementChild.replaceChildren(a);
+    }
+  }
 }
