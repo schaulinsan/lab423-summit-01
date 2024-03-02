4a4af76f87ffdcfb3a5c92ba34fe6011f74ba4fa
Dirk <drudolph@adobe.com>
2024-03-02 14:31:13 +0100

    fix: alternative quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,10 +1,8 @@
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  moveInstrumentation(quoteWrapper.firstElementChild, blockquote);
-  quoteWrapper.replaceChildren(blockquote);
+  const p = quoteWrapper.querySelector('p');
+  blockquote.append(...p.childNodes);
+  p.replaceWith(blockquote);
 }
