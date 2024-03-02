219d2be572fd167d325af856f96529aed4ddb8fb
Dirk <drudolph@adobe.com>
2024-03-02 14:43:06 +0100

    fix: multiple paragraphs

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,14 +3,17 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
-  // get the paragraph
-  const p = quoteWrapper.querySelector('p');
-  // create a new <blockquote> we will wrap it in
-  const blockquote = document.createElement('blockquote');
-  // move the instrumentation from the paragraphs parent to the block quote (if any)
-  moveInstrumentation(p.parentElement, blockquote);
-  // replace the paragraph with the block quote
-  p.parentElement.replaceWith(blockquote);
-  // and append the p to the block quote again
-  blockquote.append(p);
+  // get the paragraph and its parent
+  const par = quoteWrapper.querySelector('p');
+  if (par) {
+    const parWrapper = par.parentElement;
+    // create a new <blockquote> we will wrap it in
+    const blockquote = document.createElement('blockquote');
+    // move the instrumentation from the paragraph wrapper to the <blockquote> (if any)
+    moveInstrumentation(parWrapper, blockquote);
+    // replace the paragraph wrapper with the <blockquote>
+    parWrapper.replaceWith(blockquote);
+    // and append all quote paragraphs to the <blockquote>
+    blockquote.append(...parWrapper.children);
+  }
 }
