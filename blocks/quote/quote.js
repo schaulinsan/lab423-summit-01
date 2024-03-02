f04d243e63ae57720da2798f5ca46bfbcdb74c66
Dirk <drudolph@adobe.com>
2024-03-02 14:52:43 +0100

    fix: editable author

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,10 +1,10 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
+export default async function decorate(block) {
+  const [quoteWrapper, authorWrapper] = block.children;
 
   // get the paragraph and its parent
-  const par = quoteWrapper.querySelector('p');
+  let par = quoteWrapper.querySelector('p');
   if (par) {
     const parWrapper = par.parentElement;
     // create a new <blockquote> we will wrap it in
@@ -15,5 +15,24 @@ export default function decorate(block) {
     parWrapper.replaceWith(blockquote);
     // and append all quote paragraphs to the <blockquote>
     blockquote.append(...parWrapper.children);
+  }
+
+  // request the /authors.json index
+  par = authorWrapper.querySelector('p');
+  let quotee = par.textContent.trim();
+  let quotees = await fetch('/authors.json');
+  if (quotees.ok) {
+    // get the returned json as javascript object
+    quotees = await quotees.json();
+    // find the first entry that's title matches our quotee
+    quotee = quotees.data.find(({ title }) => title.toLowerCase() === quotee.toLowerCase());
+    if (quotee) {
+      const { path } = quotee;
+      const a = document.createElement('a');
+      a.href = path;
+      a.append(...par.childNodes);
+      moveInstrumentation(par, a);
+      par.replaceChildren(a);
+    }
   }
 }
