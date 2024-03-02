05efa5a8ceb6408ceafcf26b18f52d3551dff295
Dirk <drudolph@adobe.com>
2024-03-02 14:16:25 +0100

    fix: editable block quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,24 +1,10 @@
-export default async function decorate(block) {
-  const [quoteWrapper, authorWrapper] = block.children;
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
+  moveInstrumentation(quoteWrapper.firstElementChild, blockquote);
   quoteWrapper.replaceChildren(blockquote);
-
-  let quotee = authorWrapper.textContent.trim();
-  // request the /authors.json index
-  let quotees = await fetch('/authors.json');
-  if (quotees.ok) {
-    // get the returned json as javascript object
-    quotees = await quotees.json();
-    // find the first entry that's title matches our quotee
-    quotee = quotees.data.find(({ title }) => title.toLowerCase() === quotee.toLowerCase());
-    if (quotee) {
-      const { path } = quotee;
-      const a = document.createElement('a');
-      a.href = path;
-      a.append(...authorWrapper.firstElementChild.childNodes);
-      authorWrapper.firstElementChild.replaceChildren(a);
-    }
-  }
 }
